5029a10c713c818cf7da5337914ea794d5ef283f
Anton Schubert <anton.schubert@gmx.de>
2013-01-21 16:42:30 +0100

    Move Core Functionality to new File, optimize Animation, now working in any parent-div, ios div-scrolling, target scrollPos specifiable

---
+++ sideScroll/scripts/hAnimate.js
@@ -0,0 +1,141 @@
+function setCss(element, props) {
+	for (var key in props) {
+		element.style.setProperty(key, props[key], false);
+	}
+}
+
+function Display(view, parent) { //parent optional - default is document.body
+	this.container = document.createElement("div");
+	this.container.id = "container";
+	var parent = parent || document.body;
+	parent.appendChild(this.container);
+	
+	this.animating = false;
+	
+	setCss(this.container.parentNode, {
+		"overflow-x" : "hidden",
+		"overflow-y" : "scroll",
+		"-webkit-overflow-scrolling" : "touch"
+	});
+	
+	setCss(this.container, this.containerCss);
+	this.switchView(view);
+}
+
+Display.prototype = {
+	containerCss : { //helper object
+		"-webkit-transform-style" : "preserve-3d",
+		"-webkit-backface-visibility" : "hidden",
+		"-webkit-box-sizing" : "border-box",
+		"position" : "absolute",
+		"width" : "100%",
+	},
+	resetSize : function (heightDiff) {
+		heightDiff = 0 || heightDiff;
+		if (!this.parentIsBody) {
+			setCss(this.container.parentNode, {
+				"height" : (window.innerHeight - heightDiff) + "px",
+				"width" : (window.innerWidth) + "px"
+			});
+		}
+	},
+	switchView : function (view, callback, animate, direction, yPos) { //direction -1/+1
+		if (!view || this.animating)
+			return;
+			
+		var animate = animate || false;
+		var direction = direction || -1;
+		var yPos = yPos || 0;
+		var prevHash = window.location.hash;
+		var prevScroll = this.container.parentNode.scrollTop;
+		console.log("yPos: "+yPos);
+		
+		if (window.location.hash != view.id) {
+			window.location.hash = view.id;
+		}
+		
+		if (animate && (direction == 1 || direction == -1)) {
+			var width = window.innerWidth;
+			var move = function (element, value) {
+				setCss(element, {
+					"-webkit-transition" : "-webkit-transform .35s ease-in-out",
+					"-webkit-transform" : "translate3d(" + value + ", 0px, 0px)"
+				})
+			};
+			
+			this.animating = true;
+			
+			//create new Container
+			tmpContainer = document.createElement("div");
+			tmpContainer.id = "tmpContainer";
+			this.container.parentNode.insertBefore(tmpContainer, this.container);
+			
+			//set it's css
+			setCss(tmpContainer, this.containerCss);
+			setCss(tmpContainer, {
+				"-webkit-transform" : "translate3d(" + (-direction * 100) + "%, 0, 0)",
+				"top" : (this.container.parentNode.scrollTop - yPos) + "px"
+			});
+			
+			console.log(this.container.parentNode.scrollTop - yPos);
+			
+			//fill in enough elements for animation
+			var height = window.innerHeight;
+			var clone = view.cloneNode(true);
+			var left = clone.childNodes.length;
+			while (left-- && tmpContainer.offsetHeight < height+yPos) {
+				tmpContainer.appendChild(clone.childNodes[0]);
+			}
+			
+			//function to execute when animation finishes
+			var finishFn = function () {
+				//move tmpContainer to the right yPosition
+				tmpContainer.style.top = 0;
+				this.container.parentNode.scrollTop = yPos;
+				
+				//make tmpContainer the new container
+				var tmpRef = this.container
+				this.container = tmpContainer;
+				tmpRef.parentNode.removeChild(tmpRef);
+				this.container.id = "container";
+				
+				//fill in the the previously omitted elements
+				while (left-- > 0) {
+					tmpContainer.appendChild(clone.childNodes[0]);
+				}
+				
+				tmpContainer.removeEventListener("webkitTransitionEnd", finishFn);
+				
+				//call user-defined callback function
+				if (callback) {
+					callback(animate, prevHash, prevScroll, direction);
+				}
+				this.animating = false;
+			}
+			.bind(this)
+			
+			//wait until tmpContainer is ready
+			webkitRequestAnimationFrame(function () {
+				tmpContainer.addEventListener("webkitTransitionEnd", finishFn)
+				
+				//start animation
+				move(tmpContainer, 0);
+				move(container, (direction * 100) + "%");
+			}
+				.bind(this));
+			
+		} else {
+			//replace current container content with the views content
+			this.container.innerHTML = "";
+			var clone = view.cloneNode(true);
+			var left = clone.childNodes.length;
+			while (left--) {
+				this.container.appendChild(clone.childNodes[0]);
+			}
+			this.container.parentNode.scrollTop = yPos;
+			if (callback) {
+				callback(animate, prevHash, prevScroll);
+			}
+		}
+	}
+}
--- sideScroll/scripts/main.js
+++ sideScroll/scripts/main.js
@@ -1,4 +1,5 @@
 function init() {
+	//Set up Functions
 	var views = document.getElementsByClassName("view");
 	var getView = function (id) {
 		for (var i = 0; i < views.length; i++) {
@@ -6,140 +7,72 @@ function init() {
 				return views[i];
 		}
 	}
-	
-	var setEventListeners = function () {
-		console.log("registering event listeners");
-		document.getElementById("rightBtn").addEventListener("click", function () {
-			display.switchView(getView("view1"), setEventListeners, true, 1);
-		});
-		
-		document.getElementById("leftBtn").addEventListener("click", function () {
-			display.switchView(getView("view1"), setEventListeners, true, -1);
-		});
-		
-		document.getElementById("addRows").addEventListener("click", function () {
-			for (var i = 0; i < 300; i++) {
-				var div = document.createElement("div");
-				div.className = "listItem";
-				div.innerHTML = "<p>" + i + "</p>"
-					getView("view1").appendChild(div);
-			}
-			display.switchView(getView("view1"), setEventListeners);
-		});
-		console.log(document.getElementById("rightBtn"));
-	}
-	
-	var id = window.location.hash.split("#")[1] || "view1";
-	var display = new Display(getView(id));
-	setEventListeners();
-	
-	window.onorientationchange = function () {
-		console.log("Event: hashchange | new orientation: " + window.orientation); //possibilities: 0 90 -90
-	}
-	
-	window.onhashchange = function () {
-		console.log("Event: hashchange | new hash: " + window.location.hash); //hash with pre #
-		display.switchView(getView(window.location.hash.split("#")[1]), setEventListeners);
-	};
-}
-
-function setCss(element, props) {
-	for (var key in props) {
-		element.style.setProperty(key, props[key], false);
-		//element.style[key]=[props[key]]
-	}
-}
-
-function Display(view) {
-	this.currentView = view;
-	this.container = document.createElement("div");
-	this.container.id = "container";
-	document.body.insertBefore(this.container, document.getElementById("views"));
-	this.animating = false;
-	
-	this.switchView(view);
-	
-	setCss(document.body, {
-		"width" : "100%",
-		"overflow-x" : "hidden"
-	});
-	
-	setCss(this.container, this.containerCss);
-}
-
-Display.prototype = {
-	containerCss : { //helper object
-		"-webkit-transform-style" : "preserve-3d",
-		"-webkit-backface-visibility" : "hidden",
-		"-webkit-box-sizing" : "border-box",
-		"box-sizing" : "border-box",
-		"position" : "absolute",
-		"top" : 0,
-		"width" : "100%"
-	},
-	switchView : function (view, callback, animate, direction) { //direction -1/+1
-		console.log(view + " " + this.animating + " " + animate + " " + direction);
-		if (!view || this.animating)
-			return;
-		if (animate && (direction == 1 || direction == -1)) {
-			var width = window.innerWidth;
-			var move = function (element, value) {
-				setCss(element, {
-					"-webkit-transition" : "-webkit-transform .35s ease-in-out",
-					//"-webkit-transform": "translate3d("+value+", 0px, 0px)"
-					"-webkit-transform" : "translateX(" + value + ")"
-				})
-			};
-			
-			this.animating = true;
-			
-			//create new Container
-			tmpContainer = document.createElement("div");
-			tmpContainer.id = "tmpContainer";
-			tmpContainer.innerHTML = view.innerHTML
-				
-				//set it's css
-				setCss(tmpContainer, this.containerCss);
-			setCss(tmpContainer, {
-				//"-webkit-transform": "translate3d("+(-direction*100)+"%, 0, 0)"
-				"-webkit-transform" : "translateX(" + (-direction * 100) + "%)"
-			});
-			document.body.insertBefore(tmpContainer, this.container);
-			
-			//force visual update of the element
-			//tmpContainer.style.height="auto";
-			//var height=tmpContainer.offsetHeight;
-			//tmpContainer.style.height=tmpContainer.offsetHeight;
-			
-			//alernatively timeout delay 0
-			webkitRequestAnimationFrame(function () {
-				//start animation
-				move(tmpContainer, 0);
-				move(container, (direction * 100) + "%");
-				
-				//callback after animation ended. better use animationend event
-				setTimeout(function () {
-					var tmpRef = this.container
-						this.container = tmpContainer;
-					tmpRef.parentNode.removeChild(tmpRef);
-					this.container.id = "container";
-					if (callback) {
-						callback();
-					}
-					this.animating = false;
-				}
-					.bind(this), 500);
-			}
-				.bind(this));
-		} else {
-			this.currentView = view;
-			if (window.location.hash != this.currentView.id) {
-				window.location.hash = this.currentView.id;
-			}
-			this.container.innerHTML = this.currentView.innerHTML;
-			if (callback) {
-				callback();
-			}
+	var addToAll=function(className, type, listener){
+		var btns = document.getElementsByClassName(className);
+		for (var i = 0; i < btns.length; i++) {
+			btns[i].addEventListener(type, listener);
 		}
 	}
+	var setEventListeners = function () {
+		console.log("registering event listeners");
+		
+		addToAll("btn","click", function () {
+			backButton=false;
+			display.switchView(getView("view2"), onSwitchView, true, -1);
+		});
+		
+		addToAll("addBtn", "click", function () {
+			for (var i = 0; i < 200; i++) {
+				var div = document.createElement("div");
+				if((i%10)==0){
+					div.classList.add("button");
+					div.classList.add("btn");
+					div.innerHTML = "Button <div class='triangle-right'></div>";
+				}else{
+					div.classList.add("listItem");
+					div.innerHTML = "<p>" + i + "</p>"
+				}
+				getView("view1").appendChild(div);
+			}
+			display.switchView(getView("view1"), onSwitchView);
+		});
+	}
+	
+	var onSwitchView=function(animate, prevHash, prevScroll, direction){
+		setEventListeners();
+		if(animate && !backButton && prevHash!=window.location.hash){
+			stack.push({"direction":direction, "hash":prevHash, "scrollTop":prevScroll});
+			backButton=true;
+		}
+	}
+	
+	//Create Main Component
+	var id = window.location.hash.split("#")[1] || "view1";
+	var parent = document.getElementById("parent");
+	var backButton=true;
+	var display = new Display(getView(id), parent);
+	display.resetSize(80);
+	
+	//Create Stack
+	var stack=new Array();
+	
+	//Add EventListeners
+	setEventListeners();
+	
+	window.addEventListener("orientationchange", function () {
+		console.log("Event: orientationchange | new orientation: " + window.orientation); //possibilities: 0 90 -90
+		display.resetSize(80);
+	});
+	
+	window.addEventListener("hashchange", function () {
+		console.log("Event: hashchange | new hash: " + window.location.hash); //hash with pre #
+		if(backButton){ //differentiate between button inputs and browser controls
+			var lastSwitch=stack.pop();
+			if(window.location.hash==lastSwitch["hash"]){ //prevents chaos and destruction
+				display.switchView(getView(window.location.hash.split("#")[1]), onSwitchView, true, -lastSwitch["direction"], lastSwitch["scrollTop"]);
+			}else{ //user must've entered hash in address bar
+				display.switchView(getView(window.location.hash.split("#")[1]), onSwitchView );
+			}
+		}
+	});
 }
