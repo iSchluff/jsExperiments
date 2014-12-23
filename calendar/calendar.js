a58d41cd2300e0f113d77db2a0af2f44f14c7f48
Anton Schubert <anton.schubert@gmx.de>
2014-12-23 02:50:53 +0100

    remove all dependencies, replace icons, resize text

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,10 +1,69 @@
-/*global _:false, locale:false */
+/*global domHelper:false, locale:false, tmpl:false */
 /*jshint jquery:true, devel:true, browser:true, curly:true, latedef:true, newcap:true, eqeqeq:true, es3:true, immed:true, undef:true*/
 
 window.locale= {
   lang: "en",
   monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
   dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+};
+
+// Simple JavaScript Templating
+// John Resig - http://ejohn.org/ - MIT Licensed
+(function() {
+  var cache = {};
+
+  this.tmpl = function tmpl(str, data) {
+    // Figure out if we're getting a template, or if we need to
+    // load the template - and be sure to cache the result.
+    var fn = !/\W/.test(str) ?
+    cache[str] = cache[str] ||
+    tmpl(document.getElementById(str).innerHTML) :
+
+    // Generate a reusable function that will serve as a template
+    // generator (and which will be cached).
+    new Function("data",
+    "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+    // Introduce the data as local variables using with(){}
+    "p.push('" +
+
+    // Convert the template into pure JavaScript
+    str.replace(/[\r\t\n]/g, " ")
+    .replace(/'(?=[^%]*%>)/g,"\t")
+    .split("'").join("\\'")
+    .split("\t").join("'")
+    .replace(/<%=(.+?)%>/g, "',$1,'")
+    .split("<%").join("');")
+    .split("%>").join("p.push('") +
+    "');return p.join('');");
+
+    // Provide some basic currying to the user
+    return data ? fn(data) : fn;
+  };
+})();
+
+window.domHelper= {
+  index: function(el){
+    var siblings= el.parentNode.childNodes;
+    var index= 0;
+    for(var i= 0; i < siblings.length; i++){
+      var sibling= siblings[i];
+      if(sibling === el){
+        return index;
+      }else if(sibling.nodeType === 1){
+        index++;
+      }
+    };
+  },
+  forSiblings: function(el, func){
+    var siblings= el.parentNode.childNodes;
+    for(var i= 0; i < siblings.length; i++){
+      var sibling= siblings[i];
+      if(sibling.nodeType === 1 && sibling !== el){
+        func(sibling, i);
+      }
+    }
+  }
 };
 
 window.calendar = function(container, startDate){
@@ -14,33 +73,34 @@ window.calendar = function(container, startDate){
   var currentLayout="days";
   var date= startDate;
   var today= new Date(date);
-  var currentDecadeStart= date.getFullYear()-date.getFullYear()%10;
-  
+  var currentDecadeStart= date.getFullYear() - date.getFullYear() % 10;
+
   var events= {
-    SELECT_YEAR: "selectYear",
-    SELECT_MONTH: "selectMonth",
-    SELECT_DAY: "selectDay"
+    SELECT: "select",
   };
-  
-  var trigger= function(eventType){
-    console.log("triggered", eventType, date);
-    container.trigger(eventType, new Date(date));
+
+  var trigger= function(eventType, data){
+    if(eventType === events.SELECT && data === "day"){
+      console.log("triggered", eventType, date);
+    }
+    var event= new CustomEvent(eventType);
+    container.dispatchEvent(event);
   };
-  
-  // dayTitles don't change  
-  data.dayTitles = [];
-  _.each(locale.dayNames, function(element){
+
+  // dayTitles don't change
+  data.dayTitles= [];
+  locale.dayNames.forEach(function(name){
     data.dayTitles.push({
-      name: element.substring(0,2)
+      name: name.substring(0,3)
     });
   });
-  
+
   // methods for populating calendar with data
   var create= {
     years: function(){
       data.years = [];
-      
-      for(var i=currentDecadeStart-1; i<currentDecadeStart+11; i++){
+
+      for(var i= currentDecadeStart-1; i < currentDecadeStart + 11; i++){
         data.years.push({
           name: i,
           properties: i === today.getFullYear() ? "current" : ""
@@ -49,15 +109,15 @@ window.calendar = function(container, startDate){
     },
     months: function(){
       data.months = [];
-      
+
       var currentMonth;
       if(date.getFullYear() === today.getFullYear()){
         currentMonth = today.getMonth();
       }
-      
-      _.each(locale.monthNames, function(element, index){
+
+      locale.monthNames.forEach(function(name, index){
         data.months.push({
-          name: element.substring(0,3),
+          name: name.substring(0,3),
           properties: currentMonth === index ? "current" : ""
         });
       });
@@ -67,33 +127,33 @@ window.calendar = function(container, startDate){
     },
     days: function(){
       data.days = [];
-      
+
       // days of previous month
       var prevDays = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
       prevDays = prevDays === 0 ? 7 : prevDays;
-      
+
       var prevDayCount = this.daysInMonth(date.getFullYear(), date.getMonth()-1);
-      for(var i=prevDays; i>0; i--){
+      for(var i= prevDays; i>0; i--){
         data.days.push({
           name: (prevDayCount-i+1),
           properties: "prev"
         });
       }
-      
+
       var dayCount = this.daysInMonth(date.getFullYear(), date.getMonth());
       var day = new Date(date);
-      for(i=0;i<dayCount;i++){
+      for(i= 0; i < dayCount;i++){
         day.setDate(i+1);
-        
+
         data.days.push({
           name: day.getDate(),
           properties: day.getTime() === today.getTime() ? "current" : ""
         });
       }
-      
+
       // days of next month
       var nextDays = 42 - prevDays - dayCount;
-      for(i=0;i<nextDays;i++){ 
+      for(i= 0; i < nextDays; i++){
         data.days.push({
           name: (i+1),
           properties: "next"
@@ -101,14 +161,14 @@ window.calendar = function(container, startDate){
       }
     }
   };
-  
-  
+
+
   // Render new Calendar Template depending on Layout
   var render = function(){
     data.layout= currentLayout;
-    
+
     if(currentLayout === "years"){
-      data.title = currentDecadeStart + "-" + (currentDecadeStart+9);
+      data.title = currentDecadeStart + "-" + (currentDecadeStart + 9);
       create.years();
     }else if(currentLayout === "months"){
       data.title = date.getFullYear();
@@ -117,11 +177,11 @@ window.calendar = function(container, startDate){
       data.title = locale.monthNames[date.getMonth()] + " " + date.getFullYear();
       create.days();
     }
-    
-    container.html("");
-    container.append(_.templates.calendar(data));
+
+    container.innerHTML= "";
+    container.innerHTML= tmpl("calendarTemplate", data);
   };
-  
+
   // Handle Clicks on Left/Right Buttons (Depending on layout)
   var nextDate= function(direction){
     switch(currentLayout){
@@ -132,15 +192,16 @@ window.calendar = function(container, startDate){
       case "months":
         date.setFullYear(date.getFullYear() + direction);
         render();
-        trigger(events.SELECT_YEAR);
+
+        trigger(events.SELECT, "year");
         break;
       default:
         date.setMonth(date.getMonth() + direction);
         render();
-        trigger(events.SELECT_MONTH);
+        trigger(events.SELECT, "month");
     }
   };
-  
+
   var switchLayout= function(layout){
     // update decade
     if(layout === "years"){
@@ -153,7 +214,7 @@ window.calendar = function(container, startDate){
   // Click Event Handling
   var handleEvent= function(event){
     var el= event.target;
-    
+
     if(el.classList.contains("next")){
       nextDate(1);
     }else if(el.classList.contains("prev")){
@@ -168,48 +229,58 @@ window.calendar = function(container, startDate){
         default:
           switchLayout("months");
       }
-    
+
     }else if(el.classList.contains("year")){
-      date.setYear(+$(el).text());
+      date.setYear(el.textContent);
       switchLayout("months");
-      trigger(events.SELECT_YEAR);
-      
+      trigger(events.SELECT, "year");
+
     }else if(el.classList.contains("month")){
-      date.setMonth($(el).index());
+      date.setMonth(domHelper.index(el));
       switchLayout("days");
-      trigger(events.SELECT_MONTH);
-      
+      trigger(events.SELECT, "month");
+
     // click on already selected day
     }else if(el.classList.contains("day") && el.classList.contains("selected")){
-      $(el).removeClass("selected");
-      trigger(events.SELECT_MONTH);
-    
+      el.classList.remove("selected");
+      trigger(events.SELECT, "month");
+
     }else if(el.classList.contains("day")){
-      date.setDate(+$(el).text());
-      $(el).addClass("selected").siblings(".selected").removeClass("selected");
-      trigger(events.SELECT_DAY);
+      date.setDate(el.textContent);
+      el.classList.add("selected");
+
+      // remove selected from siblings
+      domHelper.forSiblings(el, function(sibling){
+        if(sibling.classList.contains("selected")){
+          sibling.classList.remove("selected");
+        }
+      });
+
+      trigger(events.SELECT, "day");
     }
   };
-  
+
+
   var that= {};
-  
+
   // highlights days with events
   that.setEventDays= function(eventDays){
-    _.each($(".day:not(.prev, .next)",container), function(element, index){
-      if(eventDays[index]){
-        element.classList.add("event");
+    var days= document.querySelectorAll(".calendar .day:not(.prev, .next)");
+    for(var i=0; i<days.length; i++){
+      if(eventDays[i]){
+        days[i].classList.add("event");
       }
-    });
+    }
   };
-  
+
   // requests an calendar at a specific date
   that.setDate= function(resetDate){
     date= resetDate;
     render();
   };
-  
-  container.addClass("calendar");
-  container.on("click", handleEvent);
+
+  container.classList.add("calendar");
+  container.addEventListener("click", handleEvent);
   render();
   return that;
 };
